b478b3d31bc95dee91cd36f5abc464fe66ab5981
ShahanePr <shahanepraful77@gmail.com>
2023-04-02 18:04:53 +0530

    first commit of java

---
+++ FirstCode/src/com/Programs/CoreJava/Addition.java
@@ -0,0 +1,14 @@
+package com.Programs.CoreJava;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a =10;
+		int b=20;
+		System.out.println("sum is "+ (a+b));
+	}
+
+}
